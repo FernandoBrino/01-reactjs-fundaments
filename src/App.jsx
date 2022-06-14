28be4c4b81626089dbb7bca79eddbc8ac1183560
Fernando Brino <fernandobrino11@gmail.com>
2022-06-13 22:38:19 -0300

    finished sidebar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import { Header } from "./components/Header";
 import { Post } from "./Post";
+import { Sidebar } from "./components/Sidebar";
+
+import styles from './App.module.css'
 
 import './global.css';
 
@@ -8,14 +11,19 @@ export function App() {
     <>
       <Header />
 
-      <Post 
-        author="Fernando Brino" 
-        content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio repellat repudiandae asperiores fuga quisquam ad nesciunt? Officia inventore sunt perspiciatis placeat eius sapiente. Dicta veritatis consequatur sed quam asperiores laborum!"
-      />
-      <Post 
-        author="Henrique Marques" 
-        content="A new post!"
-      />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post 
+          author="Fernando Brino" 
+          content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio repellat repudiandae asperiores fuga quisquam ad nesciunt? Officia inventore sunt perspiciatis placeat eius sapiente. Dicta veritatis consequatur sed quam asperiores laborum!"
+          />
+          <Post 
+            author="Henrique Marques" 
+            content="A new post!"
+          />
+        </main>
+      </div>
     </>
   )
 }
