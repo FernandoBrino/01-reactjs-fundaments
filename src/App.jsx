497229930af37b075c6c0f5a4a7e2a80c5fdedf8
Fernando Brino <fernandobrino11@gmail.com>
2022-06-18 13:43:34 -0300

    Create posts array to dynamic posts

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,37 @@ import styles from './App.module.css'
 
 import './global.css';
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      authorUrl: "https://github.com/FernandoBrino.png",
+      name: "Fernando Brino",
+      role: "Intern GBM"
+    },
+    content: [
+      {type: 'paragraph', content: 'Fala galeraa 👋'},
+      {type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      {type: 'link', content: 'jane.design/doctorcare'},
+    ],
+    publishedAt: new Date('2022-06-18 13:28:00')
+  },
+  {
+    id: 2,
+    author: {
+      authorUrl: "https://github.com/RodolfoNovo.png",
+      name: "Rodolfo Junior",
+      role: "Unemployed"
+    },
+    content: [
+      {type: 'paragraph', content: 'Boaaa👋'},
+      {type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz da Rocket pra aprender sobre HTML 🚀'},
+      {type: 'link', content: 'jane.design/doctorcare'},
+    ],
+    publishedAt: new Date('2022-06-17 11:28:00')
+  },
+]
+
 export function App() {
   return (
     <>
@@ -14,14 +45,15 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post 
-          author="Fernando Brino" 
-          content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio repellat repudiandae asperiores fuga quisquam ad nesciunt? Officia inventore sunt perspiciatis placeat eius sapiente. Dicta veritatis consequatur sed quam asperiores laborum!"
-          />
-          <Post 
-            author="Henrique Marques" 
-            content="A new post!"
-          />
+          {posts.map(post => {
+            return (
+              <Post 
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            );
+          })}   
         </main>
       </div>
     </>
