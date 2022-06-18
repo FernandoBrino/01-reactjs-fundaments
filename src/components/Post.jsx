08f6d87e699865acc4cce5ed7040bc22e20fd53e
Fernando Brino <fernandobrino11@gmail.com>
2022-06-18 00:40:31 -0300

    Avatar component done

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,4 @@
+import { Avatar } from './Avatar';
 import { Comment } from './Comment';
 import styles from './Post.module.css';
 
@@ -6,7 +7,7 @@ export function Post() {
         <article className={styles.post}>
             <header>
                 <div className={styles.author}>
-                    <img className={styles.avatar} src="https://github.com/FernandoBrino.png" />
+                    <Avatar src="https://github.com/FernandoBrino.png" />
                     <div className={styles.authorInfo}> 
                         <strong>Fernando Brino</strong>
                         <span> Web Developer</span>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import { PencilLine } from 'phosphor-react';
+import { Avatar } from './Avatar';
 
 import styles from './Sidebar.module.css';
 
@@ -11,7 +12,7 @@ export function Sidebar() {
             />
 
             <div className={styles.profile}>
-                <img className={styles.avatar} src="https://github.com/FernandoBrino.png" />
+                <Avatar src="https://github.com/FernandoBrino.png"/>
                 <strong>Fernando Brino</strong>
                 <span>Web Developer</span>
             </div>
